c08f93dc20949ea4d02f07628d029723a3be00d0
Nitindharmesh <nitinaakriti2670@gmail.com>
2023-12-29 11:27:37 +0530

    JAVASCRIPT_NOTES

---
+++ Practicefiles/day1.js
@@ -0,0 +1,17 @@
+// Comment can makes code readable;
+// Welcome to the 30 day of javaScript;/
+/*
+
+Comment can makes readable easy to reuse and information
+
+*/
+const first_name = "Nitin_dharmesh";
+let un;
+let me = null;
+const areYou_programmer = true;
+
+console.log(typeof first_name);
+console.log(typeof un);
+console.log(typeof me);
+console.log(typeof areYou_programmer);
+console.log("Nitin");
